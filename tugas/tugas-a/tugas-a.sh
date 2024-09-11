a259ec63d9c0d5e0501583f5a35ca76c4cb92e38
alfathanam <alfathanam@gmail.com>
2024-09-11 18:53:11 +0700

    coba hapus

--- tugas/tugas-a/tugas-a.sh
+++
@@ -1,6 +0,0 @@
-directory="/home/aris/dev-aris/bootcamp-dev-ops/sesi_4/var/log"
-echo $directory
-
-awk -v name="ERROR" '$3 == name {count++} END {print "total ERROR muncul : ", count, "kali"}' /home/aris/dev-aris/bootcamp-dev-ops/sesi_4/var/log/webserver.log
-awk -v name="401" '$4 == name {count++} END {print "total HTTP ERROR",name,"muncul: ", count, "kali"}' /home/aris/dev-aris/bootcamp-dev-ops/sesi_4/var/log/webserver.log
-awk -v name="502" '$4 == name {count++} END {print "total HTTP ERROR",name,"muncul: ", count, "kali"}' /home/aris/dev-aris/bootcamp-dev-ops/sesi_4/var/log/webserver.log
